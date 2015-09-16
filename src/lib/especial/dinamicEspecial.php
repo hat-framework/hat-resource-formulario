8065a811508bf7ca6f37d6e0d92bd1b5bf9f32e3
Cliente <Cliente@Cliente-PC>
2015-09-16 14:52:49 -0300

    requireOnceBug

--- src/lib/especial/dinamicEspecial.php
+++ src/lib/especial/dinamicEspecial.php
@@ -9,7 +9,7 @@ class dinamicEspecial extends especialInterface {
     public function js($campo, $array, $form){
         $class = $array['type'] ."Type";
         $dir   = classes\Classes\Registered::getResourceLocation('formulario', true);
-        require_once $dir."lib/types/$class.php";
+        require_once $dir."/src/lib/types/$class.php";
         
         $value = $form->getVar($campo);
         if($value == "")
